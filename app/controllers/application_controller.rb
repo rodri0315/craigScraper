43373c87846fa6578b9e1f78e3ce195a37ef51c9
Jorge Rodriguez <rodri0315@gmail.com>
2017-09-27 12:46:37 -0400

    Refactor how the listings information get saved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,20 +3,25 @@ class ApplicationController < ActionController::Base
 
   def scrape_craigslist
     @post = Post.all
-
-    @listings_count = []
-
+    # get count of first batch of listings
     @posts_200 = Post.where("price >= 0 and price <= 200 ").count
-    # Get the number of posts and save the count to listings_count array.
+
+    @apt_price_info =[]
+    # Get the number of posts and save the count, and price ranges in apt_price_info array, save information in a hash.
     # Could make this implementation much better
     j = 0
     1.upto(9) do |i|
-      # get the start price ranges to use it in the where method
+      # get the price ranges to use it in the where method and push the numbers to the apt_price_info hash.
       start = ((j+ 1) * 200) + 1
       up_to = (i+ 1) * 200
       # Get the count and save it to the array
-      posts = Post.where("price >= #{start} and price <= #{up_to}").count
-      @listings_count << posts
+      posts_count = Post.where("price >= #{start} and price <= #{up_to}").count
+      listing_info = {
+        price_start: start,
+        limit_price: up_to,
+        count: posts_count
+      }
+      @apt_price_info << listing_info
       j += 1
     end
 
